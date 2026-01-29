99b41f9f8e9592bdb219499b676d491be54ea36e
Bhavani Prasad kanduri <bunnysa4532a@gmail.com>
2026-01-29 20:40:39 +0530

    added basic list operations

---
+++ Python/01_list_basic.py
@@ -0,0 +1,8 @@
+#basic list operations
+numbers =[1,2,3,4,5]
+print("Numbers:",numbers)
+print("first element:",numbers[0])
+print("last element:",numbers[-1])
+print("length of list:",len(numbers))
+numbers.append(6)
+print("list after appending:",numbers)
