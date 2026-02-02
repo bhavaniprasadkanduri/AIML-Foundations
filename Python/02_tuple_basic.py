86500162e918e937a66c82e159061a5c18c4dc90
Bhavani Prasad kanduri <bunnysa4532a@gmail.com>
2026-02-02 14:31:02 +0530

    Added tuple basics

---
+++ Python/02_tuple_basic.py
@@ -0,0 +1,10 @@
+# basic tuple operations
+
+numbers = (10, 20, 30, 40)
+
+print("Tuple:", numbers)
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+print("Length of tuple:", len(numbers))
+
+#Tuple is immutable
