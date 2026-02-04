14e945be4cdb5d023b030b2a03708400905a22f8
Bhavani Prasad kanduri <bunnysa4532a@gmail.com>
2026-02-04 21:09:13 +0530

    Added set basics

---
+++ Python/03_set_basic.py
@@ -0,0 +1,10 @@
+# basic set operations
+
+numbers = {1, 2, 3, 4, 5}
+
+print("Set:", numbers)
+print("Length of set:", len(numbers))
+
+numbers.add(6)
+print("After adding an element:", numbers)
+#duplicates are not allowed 
