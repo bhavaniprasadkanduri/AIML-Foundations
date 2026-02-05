f85803e83ba83b432b0780c152ed20ea994691c1
Bhavani Prasad kanduri <bunnysa4532a@gmail.com>
2026-02-05 14:23:09 +0530

    Added basic array operations

---
+++ DSA/01_array_basic.py
@@ -0,0 +1,12 @@
+# basic array operations
+
+arr = [10, 20, 30, 40, 50]
+
+print("Array:", arr)
+print("First element:", arr[0])
+print("Last element:", arr[-1])
+print("Length of array:", len(arr))
+
+print("All elements:")
+for x in arr:
+    print(x)
