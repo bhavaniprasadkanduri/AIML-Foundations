47bde24089022c7a3177257f4c1e6f467e3b2d29
Bhavani Prasad kanduri <bunnysa4532a@gmail.com>
2026-02-11 11:26:40 +0530

    Added dictionary basics

---
+++ Python/04_dictionary_basic.py
@@ -0,0 +1,13 @@
+# basic dictionary operations
+
+student = {
+    "name": "Rahul",
+    "age": 20,
+    "branch": "AIML"
+}
+
+print("Dictionary:", student)
+print("Name:", student["name"])
+
+student["age"] = 21
+print("Updated age:", student["age"])
