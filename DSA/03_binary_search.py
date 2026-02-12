f33a51fc091053314bb9ad26fb2060070824f8c8
Bhavani Prasad kanduri <bunnysa4532a@gmail.com>
2026-02-12 17:43:38 +0530

    Added binary search implementation

---
+++ DSA/03_binary_search.py
@@ -0,0 +1,23 @@
+# binary search example
+
+arr = [10, 20, 30, 40, 50]
+target = 30
+
+low = 0
+high = len(arr) - 1
+found = False
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if arr[mid] == target:
+        print("Element found at index", mid)
+        found = True
+        break
+    elif arr[mid] < target:
+        low = mid + 1
+    else:
+        high = mid - 1
+
+if not found:
+    print("Element not found")
