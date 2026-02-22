d3f15a9df3e692788c44e593a8e7e74fae054702
Bhavani Prasad kanduri <bunnysa4532a@gmail.com>
2026-02-22 11:35:45 +0530

    Practiced functions with simple problems

---
+++ Python/07_function_practice.py
@@ -0,0 +1,13 @@
+# function practice problems
+
+def square(num):
+    return num * num
+
+def find_max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+print("Square of 5:", square(5))
+print("Max of 10 and 20:", find_max(10, 20))
