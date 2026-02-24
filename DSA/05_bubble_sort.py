1894525e6d523b290d2b2fb316712268fc2be25f
Bhavani Prasad kanduri <bunnysa4532a@gmail.com>
2026-02-24 15:13:44 +0530

    Added bubble sort implementation

---
+++ DSA/05_bubble_sort.py
@@ -0,0 +1,12 @@
+# bubble sort example
+
+arr = [5, 1, 4, 2, 8]
+n = len(arr)
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if arr[j] > arr[j + 1]:
+            # swap
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted array:", arr)
